2f247ae1276d2883a0110f07c6ae87d4a96b4958
kennethSoza <kenneth.soza8830@est.uca.edu.ni>
2021-11-26 16:31:12 -0600

    APIS de Medico, Perfiles, Sucursales, Tipo muestra y Tipo Orden con Controllers Completos

---
+++ WebAPI/Controllers/AreaServLabEmpleadosController.cs
@@ -0,0 +1,33 @@
+﻿using Aplicacion.AreaServLabEmpleados;
+using Dominio.Model;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AreaServLabEmpleadosController : MiControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<TblAreaServLabEmpleado>>> Get()
+        {
+            return await Mediator.Send(new Consulta.Ejecuta());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TblAreaServLabEmpleado>> Detalle(Guid id)
+        {
+            return await Mediator.Send(new ConsultaId.AreaServLabEmpleadoUnico { Id = id });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Crear(Nuevo.Ejecuta data)
+        {
+            return await Mediator.Send(data);
+        }
+    }
+}
