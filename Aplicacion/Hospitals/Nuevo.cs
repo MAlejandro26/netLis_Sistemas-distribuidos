4ddcf5e510ae028ca5d77bf2bd54077231de4104
MAlejandro26 <miguel8889solis8889@gmailcom>
2021-11-26 11:11:38 -0600

    Implementación de Hospital

---
+++ Aplicacion/Hospitals/Nuevo.cs
@@ -0,0 +1,47 @@
+﻿using Dominio;
+using Dominio.Model;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Hospitals
+{
+    public class Nuevo
+    {
+        public class Ejecuta : IRequest
+        {
+            public string Descripcion { get; set; }
+        }
+
+        public class Manejador : IRequestHandler<Ejecuta>
+        {
+            private readonly netLisContext _context;
+            public Manejador(netLisContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var h = new TblCatHospital
+                {
+                    IdHospital = Guid.NewGuid(),
+                    Descripcion = request.Descripcion,
+                    Estado = 1
+
+                };
+                _context.TblCatHospitals.Add(h);
+                var valor = await _context.SaveChangesAsync();
+                if (valor > 0)
+                {
+                    return Unit.Value;
+                }
+                throw new Exception("No se pudo guardar el hospital");
+            }
+        }
+    }
+}
